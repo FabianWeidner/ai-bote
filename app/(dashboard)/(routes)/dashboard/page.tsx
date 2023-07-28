e809a95f294aa03b195373d9a7c5eea3341767af
fabian <fabian@grapes-app.com>
2023-07-28 02:28:21 +0200

    added card to dashboard

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,8 +1,9 @@
 "use client";
+import { useRouter } from "next/navigation";
 
 import { Card } from "@/components/ui/card";
 import { cn } from "@/lib/utils";
-import { ArrowRight, MessageSquare } from "lucide-react";
+import { ArrowRight, CodeIcon, ImageIcon, MessageSquare, Music, VideoIcon } from "lucide-react";
 
 const tools = [
   {
@@ -12,8 +13,37 @@ const tools = [
     bgColor: "bg-violet-500/10",
     href: "/conversation",
   },
+  {
+    label: "Music Generation",
+    icon: Music,
+    color: "text-emerald-500",
+    bgColor: "bg-emerald-500/10",
+    href: "/music",
+  },
+  {
+    label: "Image Generation",
+    icon: ImageIcon,
+    color: "text-pink-700",
+    bgColor: "bg-pink-700/10",
+    href: "/image",
+  },
+  {
+    label: "Video Generation",
+    icon: VideoIcon,
+    color: "text-orange-700",
+    bgColor: "bg-orange-700/10",
+    href: "/video",
+  },
+  {
+    label: "Code Generation",
+    icon: CodeIcon,
+    color: "text-violet-500",
+    bgColor: "bg-violet-500/10",
+    href: "/code",
+  },
 ]
 export default function DashboardPage() {
+  const router = useRouter();
   return (
     <>
       <div className="mb-8 space-y-4">
@@ -27,6 +57,7 @@ export default function DashboardPage() {
       <div className="px-4 md:px-20 lg:px-32 space-y-4">
         {tools.map((tool) => (
           <Card
+            onClick={() => router.push(tool.href)}
             className="p-4 border-black/5 flex items-center justify-between hover:shadow-md transition cursor-pointer"
             key={tool.href}
           >
