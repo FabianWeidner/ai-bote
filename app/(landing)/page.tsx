9ef534340d5d540c8a7995ed4208da7b8878839d
fabian <fabian@grapes-app.com>
2023-07-28 02:13:51 +0200

    Commit: Introduced new UI components and added branding assets
    In this commit, several new components have been added to improve the user interface of the dashboard, which includes:
    
    Creation of Navbar.tsx and Sidebar.tsx to facilitate better navigation.
    Addition of mobile-sidebar.tsx to ensure responsiveness on mobile devices.
    Enhancement of UI with new components like card.tsx and sheet.tsx.
    Moreover, new logo assets (ai_logo.png and ai_logo2.png) have been added to the public folder to enhance branding and visual identity of the application.

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -8,12 +8,14 @@ export default function LandingPage() {
         Landing Page
       </div>
       <div className="flex gap-2">
-        <div>  <Link href="/sign-in">
-          <Button>Login</Button>
-        </Link></div>
-        <div> <Link href="/sign-up">
-          <Button>Register</Button>
-        </Link></div>
+        <div>
+          <Link href="/sign-in">
+            <Button>Login</Button>
+          </Link></div>
+        <div>
+          <Link href="/sign-up">
+            <Button>Register</Button>
+          </Link></div>
       </div>
     </section>
   )
